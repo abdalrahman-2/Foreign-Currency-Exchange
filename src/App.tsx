82b0660dc4f3165fa1957cfe450c1a25f211f77e
abdalrahman-2 <abdalrahmanesmat@outlook.com>
2026-07-15 20:32:19 +0300

    Styling the logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Logo } from './components';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1 className="text-preset-1">Hello there</h1>
+      <Logo />
     </>
   );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,10 @@
 import logo from '../assets/images/logo.svg';
+import styled from 'styled-components';
 
+const StyledImage = styled.img`
+  width: calc((139 / 16) * 1rem);
+  height: calc((29 / 16) * 1rem);
+`;
 export default function Logo() {
-  return <img src={logo} alt="FX Checker logo" />;
+  return <StyledImage src={logo} alt="FX Checker logo" />;
 }
