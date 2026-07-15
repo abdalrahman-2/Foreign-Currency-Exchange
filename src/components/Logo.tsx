f156a7f015ccae2f1473c435145e6846e0c686dc
abdalrahman-2 <abdalrahmanesmat@outlook.com>
2026-07-15 21:18:02 +0300

    Adding mobile screen query to the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,9 +2,16 @@ import logo from '../assets/images/logo.svg';
 import styled from 'styled-components';
 
 const StyledImage = styled.img`
-  width: calc((139 / 16) * 1rem);
-  height: calc((29 / 16) * 1rem);
+  width: calc(139 / 16 * 1rem);
+  height: calc(26 / 16 * 1rem);
+
+  // Mobile
+  @media (max-width: 375px) {
+    width: calc(107.15 / 16 * 1rem);
+    height: calc(20 / 16 * 1rem);
+  }
 `;
+
 export default function Logo() {
   return <StyledImage src={logo} alt="FX Checker logo" />;
 }
