e6b657c4d760017a40cb6a3c971bc22074b9a749
abdalrahman-2 <abdalrahmanesmat@outlook.com>
2026-07-27 07:40:48 +0300

    creating the history api

---
+++ src/api/apiSingleRateHistory.ts
@@ -0,0 +1,13 @@
+export default async function getSingleRateHistory(
+  base: string,
+  quote: string,
+  date: string,
+) {
+  const res = await fetch(
+    `https://api.frankfurter.dev/v2/rates?from=${date}&to=${new Date().toISOString().split('T')[0]}&base=${base}&quotes=${quote}`,
+  );
+
+  const history = await res.json();
+
+  return history;
+}
