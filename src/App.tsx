c2db5d13cb0b85d255e36b999df89bbc0072871d
abdalrahman-2 <abdalrahmanesmat@outlook.com>
2026-07-27 21:18:38 +0300

    add readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyles from './styles/GlobalStyles';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from './pages/AppLayout';
 import History from './pages/History';
@@ -31,7 +30,6 @@ function App() {
               <Route path="logs" element={<Logs />} />
             </Route>
           </Routes>
-          <ReactQueryDevtools initialIsOpen={false} />
         </BrowserRouter>
       </QueryClientProvider>
     </AppDataProvider>
